c978535ab99fc06b8ef34a27dee852625f323a3c
Sascha Nesterovic <nesterovic5@gmail.com>
2023-02-02 22:50:20 +0100

    Add crypto test files, add ability to sync with standby, update README

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # Saturn
 High Availability Service for Samba
 
-## What is Saturn?
+### What is Saturn?
+Saturn is a service that provides high availability for Samba, a popular open-source file sharing service.
+It leverages the etcd distributed key-value store to get a leader in the cluster.
 
-## How to use Saturn?
+### How to use Saturn?
 
-## Prerequirements
+### Prerequirements
 
-## Configure Saturn
+### Configure Saturn
--- saturn.sh
+++ saturn.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-export ETCDCTL_ENDPOINTS="http://<ETCD_NODE_1>:2379,http://<ETCD_NODE_2>:237"
+# Set IP-Address of Nodes that should be in the saturn cluster
+node1="<NODE_1_IPv4>"
+node2="<NODE_2_IPv4>"
+
+# Tell the system what ETCD nodes are in the ETCD cluster
+export ETCDCTL_ENDPOINTS="http://$node1:2379,http://$node2:2379"
+
+# Tell where data-dir of Samba share is
+dataDir="<FULL_PATH_SAMBA_DIR>"
 
 function log_func {
   # Get the current date
@@ -8,6 +16,7 @@ function log_func {
 
   # Create a log file in /var/log/saturn/ named saturn-log-current-date
   logfile="/var/log/saturn/saturn-log-$current_date"
+  rsynclog="/var/log/saturn/saturn_rsync-log-$current_date"
 
   # Retention policy: delete logs older than 7 days
   find /var/log/saturn/ -type f -mtime +7 -delete
@@ -15,6 +24,13 @@ function log_func {
   if [ ! -f "$logfile" ]; then
     touch "$logfile"
     echo "Created log file $logfile" >> "$logfile"
+  else
+    return
+  fi
+
+  if [ ! -f "$rsynclog" ]; then
+    touch "$rsynclog"
+    echo "Created log file $rsynclog" >> "$rsynclog"
   else
     return
   fi
@@ -37,6 +53,31 @@ function isLeader {
   fi
 }
 
+function cryptoCheck {
+  # Set SHA1SUM of test files that should be
+  sha1pdf="c8a468b702f6f72777ec9e466bf69a3e96e7dd2d"
+  sha1docx="de66e5ded334112aeb55cd21ef19c2e4ec9c8365"
+  sha1txt="da39a3ee5e6b4b0d3255bfef95601890afd80709"
+  sha1xlsx="f5ca96c15df165e0ad8a715d2976bcbd72e9f6ef"
+
+  # Calculate the sha1sum of test files in /etc/saturn
+  curPdfSha1=$(sha1sum /etc/saturn/file.pdf | awk '{print $1}')
+  curDocxSha1=$(sha1sum /etc/saturn/file.docx | awk '{print $1}')
+  curTxtSha1=$(sha1sum /etc/saturn/file.txt | awk '{print $1}')
+  curXlsxSha1=$(sha1sum /etc/saturn/file.xlsx | awk '{print $1}')
+
+  # Check if files are not encrypted
+  if [ "$sha1pdf" == "$curPdfSha1" ] && [ "$sha1docx" == "$curDocxSha1" ] && [ "$sha1txt" == "$curTxtSha1" ] && [ "$sha1xlsx" == "$curXlsxSha1" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function syncStandby {
+  rsync -av --perms --delete $dataDir root@$(etcdctl member list | grep "isLeader=false" | awk -F: '{print $2}' | awk '{print $1}' | cut -c 6-):$dataDir >> $rsynclog
+}
+
 function check_health {
   # Get the status of all nodes in the etcd cluster
   cluster_status=$(etcdctl endpoint health)
@@ -52,6 +93,12 @@ function check_health {
 while true; do
   log_func
   isLeader
-  sleep 1
+  if [[ $? -eq 0 ]]; then
+    cryptoCheck
+    if [[ $? -eq 10 ]]; then
+      syncStandby
+    fi
+  fi
+#  sleep 1
   # check_health
 done
