746d77d662b21eb29e3f8d1a75b8ae1799826c86
Sascha Nesterovic <nesterovic5@gmail.com>
2023-02-13 16:20:06 +0100

    Add backup script

---
+++ backup_saturn.sh
@@ -0,0 +1,29 @@
+virtual_ip="<VIRTUAL_IP>"
+dataDir="<FULL_PATH_SAMBA_DIR>"
+backupDir="<FULL_PATH_BACKUP_DIR>"
+
+option=$1
+
+if [ $option = "--daily"]; then
+  if [ ! -d "$backupDir/daily" ]; then
+    mkdir "$backupDir/daily"
+  fi
+  rsync -av --perms --delete root@$virtual_ip:$dataDir $backupDir/daily
+  find $backupDir/daily/ -type f -mtime +1 -delete
+fi
+
+if [ $option = "--weekly"]; then
+  if [ ! -d "$backupDir/weekly" ]; then
+    mkdir "$backupDir/weekly"
+  fi
+  rsync -av --perms --delete root@$virtual_ip:$dataDir $backupDir/weekly
+  find $backupDir/weekly/ -type f -mtime +7 -delete
+fi
+
+if [ $option = "--monthly"]; then
+  if [ ! -d "$backupDir/monthly" ]; then
+    mkdir "$backupDir/monthly"
+  fi
+  rsync -av --perms --delete root@$virtual_ip:$dataDir $backupDir/monthly
+  find $backupDir/weekly/ -type f -mtime +30 -delete
+fi
